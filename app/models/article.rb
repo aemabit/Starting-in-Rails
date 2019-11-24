f6529524c119e69a3e7bcc5ffa21c7c040765073
Andres Mejias <aemabit@gmail.com>
2019-11-24 12:41:19 -0500

    rendering form for comments in articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
     has_many :comments
     validates :title, presence: true,
-                      length: { minimum: 5 
-  end
+                      length: { minimum: 5 }
+end
   
